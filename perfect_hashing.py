8bb6880d17f9e61eb1b9e49ddc66a93886e6b28f
Alexey Dral <aadral@bigdatateam.org>
2021-11-17 00:02:35 +0300

    update interface for python.perfect_hashing programming assignment

--- perfect_hashing.py
+++ perfect_hashing.py
@@ -8,6 +8,18 @@ MIN_INT = -2 ** (8 * LONG_SIZE_IN_BYTES - 1)
 MAX_TRIALS = 4
 
 
+class HashError(Exception):
+    pass
+
+
+class PerfectHashFirstLevelError(HashError):
+    pass
+
+
+class PerfectHashSecondLevelError(HashError):
+    pass
+
+
 class HashTableChain:
     __slots__ = ("hash_function", "collection_size", "counts", "hash_table")
 
@@ -27,16 +39,67 @@ class HashTableChain:
 
 class HashTablePerfect:
     """build static hash table with < 4n memory space with universal hash family"""
-    __slots__ = ("hash_first_level", "hash_second_level", "collection_size", "counts", "hash_table")
+    __slots__ = (
+        "hash_first_level", "hash_second_level",
+        "hash_function_counter", "hash_function_counter_per_bucket",
+        "max_trials_first_level", "max_hash_function_counter",
+        "collection_size", "counts", "hash_table",
+    )
 
-    def __init__(self, collection: List[int], max_trials=MAX_TRIALS):
+    def __init__(self, collection: List[int], max_trials_first_level=MAX_TRIALS, max_hash_function_counter=None):
         self.collection_size = len(collection)
+        self.max_trials_first_level = max_trials_first_level
+        self.max_hash_function_counter = max_hash_function_counter or 2 * self.collection_size
+
+        self.counts = array.array('l', [])
+        self.hash_function_counter = 0
+        self.hash_function_counter_per_bucket = array.array('l', [])
+
         self.fill_hash_table(collection)
 
     def fill_hash_table(self, collection: List[int]):
-        # TODO: implement me and use array.array
-        raise ValueError("cannot build perfect hash table within %s trials" % MAX_TRIALS)
+        for _ in range(self.max_trials_first_level):
+            self.hash_first_level = self.generate_new_hash_function()
+            self.hash_second_level = [hash] * self.collection_size
+            self._fill_first_level_counts(collection)
+            if self._is_big_memory_consumption():
+                continue
+
+            self._fill_second_level(collection)
+            return
+
+        raise PerfectHashFirstLevelError("cannot build perfect hash table within %s trials" % self.max_trials_first_level)
+
+    def generate_new_hash_function(self):
+        # TODO: implement me
+        hash_function = lambda item: 0
+
+        self.hash_function_counter += 1
+        if self.hash_function_counter >= self.max_hash_function_counter:
+            raise PerfectHashSecondLevelError("cannot build perfect hash table with %s hash functions" % self.max_hash_function_counter)
+
+        return hash_function
+
+    def _fill_first_level_counts(self, collection: List[int]):
+        # TODO: implement me
+        self.counts = array.array('l', [0] * self.collection_size)
+
+    def _is_big_memory_consumption(self) -> bool:
+        # TODO: implement me
+        return True
+
+    def _fill_second_level(self, collection: List[int]):
+        self.hash_table = []
+        for _ in range(self.collection_size):
+            self.hash_table.append(array.array('l', []))
+
+        # TODO: populate hash_function_counter_per_bucket
+        self.hash_function_counter_per_bucket = array.array('l', [0] * self.collection_size)
+        # TODO: implement me
+        # you can use HashTableChain as temporary intermediate helper collection for buckets
+        # in this case, do not forget to specify correct hash_function for it
+        # hash_table_chain = HashTableChain(collection, hash_function=self.hash_first_level)
 
     def __contains__(self, item) -> bool:
         # TODO: implement me
-        pass
+        return False
