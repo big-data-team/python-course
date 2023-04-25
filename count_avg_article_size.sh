c933a9d870683cf83d5a54f0e265b9af64d3b7c8
Alexey Dral <aadral@bigdatateam.org>
2023-04-25 18:53:39 +0600

    add template files for HW02:Unix CLI.Base

---
+++ count_avg_article_size.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+set -x
+
+# $dataset_fpath is defined via export
+your solution is here
+echo $avg_article_size
---
+++ count_avg_topic_article_size.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+set -x
+
+# $dataset_fpath and $topic are defined via export
+your solution is here
+echo $avg_topic_article_size
