1e548aa2ef9690a5f0765ea5a070660bd8f8fe7d
a-zhenya <eadishchev@gmail.com>
2023-11-23 21:07:39 +0400

    Create github-repository-tool.sh

---
+++ github-repository-tool.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/bash
+
+USER=big-data-team
+REP_NAME="$1"
+
+# check args
+if [ -z "$REP_NAME" ]
+then
+    echo Usage: `basename $0` REPOSITORY_NAME
+    exit 1
+fi
+
+if [ -z "$TOKEN" ]
+then
+    echo Set env variable TOKEN to github token
+    exit 1
+fi
+
+# check if rep exist
+REP_ID=`curl -s -H "Authorization: Bearer $TOKEN" https://api.github.com/repos/$USER/$REP_NAME | jq '.id'`
+if [ "$REP_ID" == "null" ]
+then
+    EXIST=0
+else
+    EXIST=1
+fi
+
+if [ "$EXIST" == 0 ]
+then
+    # create
+    echo creating $USER/$REP_NAME
+    curl -s -X POST -H "Authorization: Bearer $TOKEN" https://api.github.com/orgs/$USER/repos -d "{\"name\": \"$REP_NAME\", \"description\": \"The test github API\"}" -H "Content-Type: applicaton/json"
+else
+    # delete
+    echo deleting $USER/$REP_NAME
+    curl -s -X DELETE -H "Authorization: Bearer $TOKEN" https://api.github.com/repos/$USER/$REP_NAME
+fi
